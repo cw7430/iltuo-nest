d0a25928f72e4cb2ae7891cc81d39dcea189d25a
cw7430 <cw282@naver.com>
2026-04-24 21:48:47 +0900

    auth 스키마 테이블 수정

--- drizzle.config.js
+++ drizzle.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
   dialect: 'mysql',
   schema: './src/modules/database/schemas/**/*.ts',
   out: './drizzle',
-
   dbCredentials: {
     host: process.env.DB_HOST ?? 'localhost',
     port: Number(process.env.DB_PORT) ?? 3306,
@@ -15,4 +14,6 @@ module.exports = defineConfig({
     password: process.env.DB_PASSWORD ?? '',
     database: process.env.DB_NAME ?? 'iltuo',
   },
+  verbose: true,
+  strict: true,
 });
